0ff8a4737819e9490fe1f294ecf8d40b89140610
tony-sparks <tonygyamena1@gmail.com>
2023-12-12 22:09:33 +0000

    first javascript code

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-//console.log("Hello World");
+// Cart
+let cartIcon  = document.querySelector("#cart-icon");
+let cart  = document.querySelector(".cart");
+let closeCart  = document.querySelector("#close-cart");
 
-let name = "Tony";
-console.log(name);
+//open Cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+};
+
+//close Cart
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+
